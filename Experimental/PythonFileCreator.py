d5203137de5a678159bcfb9fa42013b858b14db4
CAP1Sup <xmanpiper@gmail.com>
2019-10-22 14:22:50 -0400

    Did more work on PythonFileCreator

--- Experimental/PythonFileCreator.py
+++ Experimental/PythonFileCreator.py
@@ -6,11 +6,17 @@ from datetime import date
 
 def main():
     name = input("Enter your name: ")
-    date = date.today()
-    input("Enter your name: ")
+    # Needs to be fixed
+    currentDate = date.today()
+    purpose = input("Enter what the program will do: ")
+    
+    # Convert the stuff
+    name = "# " + name + "\n"
+    currentDate = "# " + currentDate + "\n"
+    purpose = "# " + purpose + "\n"
 
+    total = name + currentDate + purpose
 
-
-
+    print(total)
 
 main()
