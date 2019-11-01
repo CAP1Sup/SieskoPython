229349d5bf1afd2c7e411499008f44cf775b1c46
CAP1Sup <xmanpiper@gmail.com>
2019-11-01 14:23:13 -0400

    Added the first and second lab in functions unit

---
+++ Labs/GradeAverage.py
@@ -0,0 +1,60 @@
+# Christian Piper
+# 11/1/19
+# This program will accept 5 test grades (in percentage) and find the letter and average of all of the grades
+
+def main():
+    
+    namings = ["first", "second", "third", "fourth", "fifth"]
+    result = ["", "", "", "", ""]
+
+    for entry in range(0,5):
+        result[entry] = input("Input your " + namings[entry] + " test grade: ")
+        result[entry] = convertToInt(result[entry])
+        print("Your grade was a " + str( getLetterGrade( result[entry] ) ))
+
+    averageResult = calcAverage(result, 5, 2)
+
+    print("The average grade was " + str(averageResult) + "%")
+    print("The letter grade for the average was " + str( getLetterGrade(averageResult) ))
+
+def convertToInt(x):
+    try:
+        x = int(x)
+    except:
+        print("Issue converting! Make sure you are inputing valid numbers from 0-100!")
+    return x
+
+def calcAverage(results, num, decPlaces):
+
+    total = 0
+
+    for entry in range(0, num):
+        total = total + results[entry]
+
+    average = round(total / num, decPlaces)
+
+    return average
+
+def getLetterGrade(val):
+
+    try: 
+        val = int(val)
+        
+        # Count the number of each score
+        if val < 60:
+            grade = "F"
+        elif val < 70:
+            grade = "D"
+        elif val < 80:
+            grade = "C"
+        elif val < 90:
+            grade = "B"
+        else:
+            grade = "A"
+
+        return grade
+
+    except:
+        print("Conversion error!")
+
+main()
---
+++ Labs/MathFunctions.py
@@ -0,0 +1,43 @@
+# Christian Piper
+# 11/1/19
+# This program will have the functions for add, subtract, multiply, divide, and modulus
+
+def main():
+    z = input("Enter the first value: ")
+    x = input("Enter the second value: ")
+
+    namings = ["sum", "difference", "product", "quotent", "modulus"]
+    results = [add(z,x), subtract(z,x), multiply(z,x), divide(z,x), modulus(z,x)]
+    
+    for entry in range(0,5):
+        print("The " + namings[entry] + " was " + str(results[entry]))
+    
+
+def convertToInt(x):
+    try:
+        x = int(x)
+    except:
+        print("Issue converting!")
+    return x
+
+def add(z, x):
+    result = convertToInt(z) + convertToInt(x)
+    return result
+
+def subtract(z, x):
+    result = convertToInt(z) - convertToInt(x)
+    return result
+
+def multiply(z, x):
+    result = convertToInt(z) * convertToInt(x)
+    return result
+
+def divide(z, x):
+    result = convertToInt(z) / convertToInt(x)
+    return result
+
+def modulus(z, x):
+    result = convertToInt(z) % convertToInt(x)
+    return result
+
+main()
